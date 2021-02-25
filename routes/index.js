513d1086f4a0e8054360ebc550396c3034aad164
Francois Roland <froland@froland.be>
2021-02-25 23:46:46 +0100

    Simplify.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,31 @@ const express = require('express');
 const router = new express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Examen' });
+const courses = [
+  {
+    title: 'Physique',
+    teacher: 'Isaac NEWTON',
+  },
+  {
+    title: 'Physique quantique',
+    teacher: 'Albert EINSTEIN',
+  },
+  {
+    title: 'Chimie',
+    teacher: 'Antoine LAVOISIER',
+  },
+  {
+    title: 'Anatomie',
+    teacher: 'André VESALE',
+  },
+  {
+    title: 'Philosophie',
+    teacher: 'René DESCARTES',
+  },
+];
+
+router.get('/courses', (req, res, next) => {
+  res.json(courses);
 });
 
 module.exports = router;
